12ab7c739a2f17d7ea73b80361ce7b8d89fc91b1
jaypeetan <jaypee.tan@codegum.ph>
2019-08-31 22:15:45 +0800

    added the cart icon

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -8,9 +8,12 @@ export class Header extends Component {
                     <div className="container-fluid">
                         <div className="py-2 text-white">
                             <img src="https://instagram.fmnl4-2.fna.fbcdn.net/vp/5e75a07cb8a1426b30294480cad24005/5DF66611/t51.2885-19/s150x150/38458106_1783732921747920_1659467988992000000_n.jpg?_nc_ht=instagram.fmnl4-2.fna.fbcdn.net" className="header-logo"/>
-                            <span className="h1">ShakaSwim</span>
-                            <span className="h3 float-right px-1">Login</span>
-                            <span className="h3 float-right px-1">Register</span>
+                            <span className="header-title">ShakaSwim</span>
+                            <div className="float-right">
+                                <a href="#" className="text-light"><span className="px-2 header-secondary">Login</span></a>
+                                <a href="#" className="text-light"><span className="px-2 header-secondary">Register</span></a>
+                                <a href="#" className="text-light"><i className="fas fa-shopping-cart fa-2x px-2 py-3"></i></a>
+                            </div>
                         </div>
                     </div>
                 </div>

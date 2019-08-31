c1d65f6927f258a4176de38d72e1ede9a2284c3d
jaypeetan <jaypee.tan@codegum.ph>
2019-08-31 15:15:56 +0800

    Updates

---
+++ precache-manifest.19ed21929321e6eda7540fa52807715e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5457ab077615aeac4dd76e86662bc10a",
+    "url": "/ShakaSwimwear/index.html"
+  },
+  {
+    "revision": "1dc5299f242e047bb5f2",
+    "url": "/ShakaSwimwear/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c08659635c1745a2ec60",
+    "url": "/ShakaSwimwear/static/js/2.23904c75.chunk.js"
+  },
+  {
+    "revision": "1dc5299f242e047bb5f2",
+    "url": "/ShakaSwimwear/static/js/main.489ba79e.chunk.js"
+  },
+  {
+    "revision": "22c351341d55c115951d",
+    "url": "/ShakaSwimwear/static/js/runtime~main.c097168a.js"
+  }
+]);
